964c55bf5de9b3ab6a01dcf97c32704e01b68572
ErnestDuo <ayewernest@gmail.com>
2024-05-18 10:21:57 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PasswordGenerator from "../src/Components/PasswordGenerator";
+
+function App() {
+  return (
+    <div className="app">
+      <PasswordGenerator />
+    </div>
+  );
+}
+
+export default App;
